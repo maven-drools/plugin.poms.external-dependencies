7a507cbdc165bfe0dfa69f3ebf07883f148358f7
Ansgar Konermann <ansgar.konermann@googlemail.com>
2011-01-27 00:23:39 +0100

    More Module Refactoring. Extracting maven-verifier-testng, drools-assertions

--- maven-drools-plugin/src/test/java/de/lightful/maven/plugins/drools/integrationtests/CanCompileMinimumDrlFileTest.java
+++ maven-drools-plugin/src/test/java/de/lightful/maven/plugins/drools/integrationtests/CanCompileMinimumDrlFileTest.java
@@ -18,90 +18,35 @@
 package de.lightful.maven.plugins.drools.integrationtests;
 
 import de.lightful.maven.plugins.testing.ExecuteGoals;
+import de.lightful.maven.plugins.testing.MavenVerifierTest;
 import de.lightful.maven.plugins.testing.VerifyUsingProject;
-import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
-import org.apache.maven.it.util.ResourceExtractor;
 import org.drools.core.util.DroolsStreamUtils;
 import org.drools.definition.KnowledgePackage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
-import static org.fest.assertions.Fail.fail;
 
 @Test
 @VerifyUsingProject("compile_single_file")
 @ExecuteGoals("clean")
-public class CanCompileMinimumDrlFileTest {
+public class CanCompileMinimumDrlFileTest extends MavenVerifierTest {
 
   private Logger log = LoggerFactory.getLogger(CanCompileMinimumDrlFileTest.class);
 
   private static final String DROOLS_KNOWLEDGE_PACKAGE_EXTENSION = ".dkp";
   private static final String EXPECTED_OUTPUT_FILE = "target/plugintest.artifact-1.0.0" + DROOLS_KNOWLEDGE_PACKAGE_EXTENSION;
 
+  @Inject
   private Verifier verifier;
-
-  @BeforeMethod
-  private void setUpVerifier(Method testMethod) {
-    final String testDirectoryName = obtainTestDirectoryName(testMethod);
-    File testDirectory;
-    try {
-      testDirectory = ResourceExtractor.simpleExtractResources(getClass(), testDirectoryName);
-      verifier = new Verifier(testDirectory.getAbsolutePath());
-      String[] goals = obtainGoalsToExecute(testMethod);
-      assertThat(goals.length).as("Number of goals to execute").isGreaterThan(0);
-      for (String goal : goals) {
-        verifier.executeGoal(goal);
-      }
-    }
-    catch (IOException e) {
-      fail("Unable to extract integration test resources from directory '" + testDirectoryName + "'.", e);
-    }
-    catch (VerificationException e) {
-      fail("Unable to construct Maven Verifier from project in directory " + testDirectoryName + ".", e);
-    }
-  }
-
-  private String[] obtainGoalsToExecute(Method testMethod) {
-    List<String> goals = new ArrayList<String>();
-    final Class<?> declaringClass = testMethod.getDeclaringClass();
-    final ExecuteGoals annotationOnClass = declaringClass.getAnnotation(ExecuteGoals.class);
-    if (annotationOnClass != null) {
-      goals.addAll(Arrays.asList(annotationOnClass.value()));
-    }
-    final ExecuteGoals annotationOnMethod = testMethod.getAnnotation(ExecuteGoals.class);
-    if (annotationOnMethod != null) {
-      goals.addAll(Arrays.asList(annotationOnMethod.value()));
-    }
-    return goals.toArray(new String[goals.size()]);
-  }
-
-  private String obtainTestDirectoryName(Method testMethod) {
-    final VerifyUsingProject annotationOnMethod = testMethod.getAnnotation(VerifyUsingProject.class);
-    if (annotationOnMethod != null) {
-      return annotationOnMethod.value();
-    }
-
-    final Class<?> declaringClass = testMethod.getDeclaringClass();
-    final VerifyUsingProject annotationOnClass = declaringClass.getAnnotation(VerifyUsingProject.class);
-    if (annotationOnClass == null) {
-      throw new IllegalArgumentException("No @" + VerifyUsingProject.class.getSimpleName() + " annotation found on " +
-                                         "test method or test class. Don't know where to take project definition from.");
-    }
-    return annotationOnClass.value();
-  }
 
   @Test
   public void testCanCallCleanGoal() throws Exception {
